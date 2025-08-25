2aad664203c667b5d133e0ee561dbc02de24a623
Muskan-23456789 <muskanmashrak654@gmail.com>
2025-08-25 15:40:56 +0530

    do

--- muskan089/index.js
+++ muskan089/index.js
@@ -18,5 +18,5 @@ app.use("/api/blogs", blogRoutes);
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on ${PORT}`);
 });
