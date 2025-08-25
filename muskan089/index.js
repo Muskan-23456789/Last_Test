4e8e7ad0ce30e8959f2a0314b3150c7ee0ec0187
Muskan-23456789 <muskanmashrak654@gmail.com>
2025-08-25 16:17:56 +0530

    all done

--- muskan089/index.js
+++ muskan089/index.js
@@ -12,11 +12,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Hello Muskan!");
+});
+
 app.use("/api/auth", authRoutes);
 app.use("/api/blogs", blogRoutes);
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on ${PORT}`);
+  console.log(`Hello Muskan! Server is running successfully${PORT}`);
 });
