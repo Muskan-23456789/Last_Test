36647d2c0e006505bd0e8cbc147717321d41ee3f
Muskan-23456789 <muskanmashrak654@gmail.com>
2025-08-25 15:37:04 +0530

    do

--- muskan089/config/cloudinaryConfig.js
+++ muskan089/config/cloudinaryConfig.js
@@ -1,52 +1,11 @@
-import multer from 'multer';
-import { CloudinaryStorage } from 'multer-storage-cloudinary';
-import Cloudinary from './cloudinaryConfig.js';
+import { v2 as cloudinary } from 'cloudinary';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const storage = new CloudinaryStorage({
-    cloudinary,
-  params: {
-    folder: 'uploads',  
-    allowed_formats: ['jpg', 'png', 'jpeg', 'webp']
-  }
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key:    process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-const upload = multer({ storage });
-
-export default upload;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+export default cloudinary;
