f5c547dff6f317b461810109985a9e57a764d1cb
Muskan-23456789 <muskanmashrak654@gmail.com>
2025-08-25 15:33:46 +0530

    do

--- muskan089/config/cloudinaryConfig.js
+++ muskan089/config/cloudinaryConfig.js
@@ -1,8 +1,8 @@
 import multer from 'multer';
 import { CloudinaryStorage } from 'multer-storage-cloudinary';
-import cloudinary from './cloudinaryConfig.js';
+import Cloudinary from './cloudinaryConfig.js';
 
-const storage = new cloudinaryStorage({
+const storage = new CloudinaryStorage({
     cloudinary,
   params: {
     folder: 'uploads',  
--- muskan089/routes/blogRoutes.js
+++ muskan089/routes/blogRoutes.js
@@ -1,32 +1,12 @@
 import express from "express";
-import multer from "multer";
-import {createblog,getblogs,getblogById} from "../controllers/blogController.js";
-import {protect} from "../middlewares/authMiddleware.js";
+import { createBlog, getBlogs, getBlogById } from "../controllers/blogController.js";
+import { protect } from "../middlewares/authMiddleware.js";
+import upload from "../middlewares/upload.js";
 
 const router = express.Router();
-const upload = multer({ dest: "uploads/" });
 
-router.post("/",protect,upload.single("image"),createblog);
-router.get("/",getblogs);
-router.get("/:id",getblogById);
+router.post("/", protect, upload.single("image"), createBlog);
+router.get("/", getBlogs);
+router.get("/:id", getBlogById);
+
 export default router;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
